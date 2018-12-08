049dd13724be24f597548dc552fdcde9a2c047ab
Your Name <you@example.com>
2018-12-08 18:44:08 +0400

    mmmmmmmmmmm

--- index.js
+++ index.js
@@ -21,9 +21,7 @@
 
 // });
 
- var express = require("express");
- var app = express();
-
+ 
 // app.get("/", function(req, res){
 //     res.send("<h1>Hello world</h1>");
 //  });
@@ -35,15 +33,52 @@
 //     console.log("Example is running on port 3000");
 //  });
 
+// var express = require("express");
+//  var app = express();
 
-app.get("/google", function(req, res){
-    res.redirect('http://google.com');
- });
 
- app.get("/google/:search", function(req, res){
-    var search = req.params.search;
-    res.redirect('http://google.com/search?q=' + search);
- });
-  app.listen(3000, function(){
-    console.log("Example is running on port 3000");
- });
+// app.get("/google", function(req, res){
+//     res.redirect('http://google.com');
+//  });
+
+//  app.get("/google/:search", function(req, res){
+//     var search = req.params.search;
+//     res.redirect('http://google.com/search?q=' + search);
+//  });
+//   app.listen(3000, function(){
+//     console.log("Example is running on port 3000");
+//  });
+
+// var Square = require("./module");
+// var mySquareObject = new Square(5);
+
+// function main() {
+//    console.log(mySquareObject.getArea());
+// }
+// main();
+
+
+
+// var fs = require('fs');
+
+// function main(){
+//    fs.writeFile("hello.txt", "Hello world\n", function(err){
+//        console.log("fs.writeFile ended");
+//    });
+//    console.log("fs.writeFile");
+// }
+// main();
+
+// var fs = require('fs');
+// var dummyText = "Apple yep";
+
+// function main() {
+//    fs.writeFileSync("dummytext.txt", dummyText);
+//    var text = fs.readFileSync("dummytext.txt").toString();
+//    console.log(dummyText == text);
+//    console.log(text);
+//    fs.writeFileSync("undummytext.txt",
+//        text.replace("Apple", "Microsoft")
+//    );
+// }
+// main();
